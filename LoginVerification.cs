1ef488d775156d94987aaa50be13542a23d3791d
dheeraj8328 <104424282+dheeraj8328@users.noreply.github.com>
2024-12-25 23:41:24 +0530

    Add project files.

---
+++ LoginVerification.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart
+{
+    class LoginVerification
+    {
+        private SqlConnection _connection;
+        private Users users;
+
+        public LoginVerification(SqlConnection connection)
+        {
+            _connection = connection;
+            users = new Users(_connection);
+        }
+
+        public bool UserVerification()
+        {
+            Console.WriteLine("Welcome to shopping.com");
+            Console.WriteLine("Do you have the credentials?(Yes/No)");
+            string userInput = Console.ReadLine().ToLower();
+            if(userInput == "yes")
+            {
+                return Users.UserLogin();
+            }
+            else if(userInput == "no")
+            {
+                return Users.UserRegister();
+            }
+            else
+            {
+                Console.WriteLine("Enter valid Input(Yes/No)....!");
+                return false;
+            }
+
+        }
+
+    }
+}
---
+++ Users.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart
+{
+    class Users
+    {
+        private string _name;
+        private string _username;
+        private string _password;
+        private string _mobilenumber;
+        private SqlConnection _connection;
+
+        public Users(SqlConnection connection)
+        {
+            _connection = connection;
+
+        }
+        public string Name {
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _name = value;
+                }
+                else
+                {
+                    Console.WriteLine("Name Can't be Blank");
+                }
+            }
+            get
+            {
+                return _name;
+            }
+        }
+        public string Username {
+            set 
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _username = value;
+                }
+                else
+                {
+                    Console.WriteLine("Username Can't be Blank");
+                }
+            }
+            get
+            {
+                return _username;
+            }
+        }
+        public string Password {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _password = value;
+                }
+                else
+                {
+                    Console.WriteLine("Password Can't be Blank");
+                }
+            }
+            get
+            {
+                return _password;
+            }
+
+        }
+        public string MobileNumber {
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _mobilenumber = value;
+                }
+                else
+                {
+                    Console.WriteLine("Mobile Number Can't be Blank");
+                }
+            }
+            get
+            {
+                return _mobilenumber;
+            }
+        }
+
+        public bool UserLogin(string username, string password)
+        {
+            return true;
+        }
+
+        public bool UserRegister()
+        {
+            return false;
+        }
+    }
+}
